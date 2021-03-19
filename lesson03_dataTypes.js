978add66f73e400ca9477f380d333fdc95a58b77
pmita <panos.mita2@outlook.com>
2021-03-19 12:31:14 +0000

    data types explained

---
+++ lesson03_dataTypes.js
@@ -0,0 +1,27 @@
+//Primitive data types
+
+const { type } = require("os");
+
+//Numbers
+const day = 5;
+console.log(typeof day); //Prints number
+//Strings
+const name = 'Panos';
+console.log(typeof name); //Prints string
+
+//Booleans represents either true or false value
+const isLogged = true;
+console.log(typeof isLogged); //Prints boolean
+
+//Undefined represents a variable that a value hasn't been assigned to it yet
+let emptyVariable;
+console.log(emptyVariable); //Prints undefined
+
+//Null represeents a value of nothing
+let minute = null;
+
+//Symbol
+
+//Object
+
+
